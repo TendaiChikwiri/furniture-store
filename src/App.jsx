ea3707d2bb838d55836614bf54f756ba8ec72bbc
Tendai Chikwiri <tfchikwiri@gmail.com>
2021-08-14 15:24:29 +0200

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <h1>Furniture Store</h1>
+    </div>
+  );
+}
+
+export default App;
