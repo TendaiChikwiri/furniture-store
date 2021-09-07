34a3f21f1a3a182a9eff56c97e26872c3be1d95e
Tendai Chikwiri <tfchikwiri@gmail.com>
2021-09-07 21:17:41 +0200

    Checkout Page styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,22 @@
-import React from 'react';
-import "../styles/Header.scss"
+import React from "react";
+import "../styles/Header.scss";
 import { Link } from "react-router-dom";
-import { FiShoppingCart } from "react-icons/fi"
+import CartIcon from "../assets/images/cart-icon.svg";
+
 
 const Header = ({ cart }) => {
-  const cartTotal = (total, item) => total + item.count
+  const cartTotal = (total, item) => total + item.count;
   return (
     <nav className="nav-bar">
       <Link className="nav-item" to="/">
         <h2>Furniture</h2>
       </Link>
       <Link className="nav-item" to="/checkout">
-        <FiShoppingCart />
+        <img className="cart-icon" src={CartIcon} alt="" />
         {cart.length ? cart.reduce(cartTotal, 0) : "0"}
       </Link>
     </nav>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,23 +1,34 @@
 import React from 'react'
-import "../styles/Checkout.scss"
+import "../styles/Checkout.scss";
+import deleteIcon from "../assets/images/bin-icon.png"
 const Checkout = ({ cart, addToCart, reduceFromCart, deleteFromCart }) => {
   return (
     <main>
-      {cart.length === 0 ? <p>No items in cart.</p> : null}
-      <ul className="items-list">
-        {cart.map((item) => (
-          <li key={item.id} className="item">
-            <h2>{item.name}</h2>
-            <div className="more-less">
-              <button onClick={() => reduceFromCart(item.id)}>-</button>
-              <p>{item.count}</p>
-              <button onClick={() => addToCart(item)} >+</button>
-            </div>
-            <p>&#36;{item.count * item.price}</p>
-            <button onClick={() => deleteFromCart(item.id)} >Remove from Cart</button>
-          </li>
-        ))}
-      </ul>
+      {cart.length === 0 ? <p className="empty-cart">No items in cart</p> : null}
+      <div className="cart">
+        <ul className="items-list">
+          {cart.map((item) => (
+            <li key={item.id} className="item">
+              <div className="img-container">
+                <img src={item.img} alt="" />
+              </div>
+              <div className="item-details">
+                <h3 className="item-name">{item.name}</h3>
+                <div className="more-less">
+                  <button onClick={() => reduceFromCart(item.id)}>-</button>
+                  <p>{item.count}</p>
+                  <button onClick={() => addToCart(item)} >+</button>
+                </div>
+                <p>&#36;{item.count * item.price}</p>
+                <button className="delete-btn" onClick={() => deleteFromCart(item.id)} ><img src={deleteIcon} alt="" /></button>
+              </div>
+            </li>
+          ))}
+        </ul>
+        <div className="total">
+
+        </div>
+      </div>
     </main>
   )
 }
